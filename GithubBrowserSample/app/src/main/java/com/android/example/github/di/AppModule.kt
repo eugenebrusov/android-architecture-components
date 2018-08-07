c4e1393d4ab5c7e486a18a90d9bbe021b48915b5
eugenebrusov <eugenebrusov@gmail.com>
2018-08-07 22:48:46 +0300

    Remove GitHubDb

--- GithubBrowserSample/app/src/main/java/com/android/example/github/di/AppModule.kt
+++ GithubBrowserSample/app/src/main/java/com/android/example/github/di/AppModule.kt
@@ -16,36 +16,9 @@
 
 package com.android.example.github.di
 
-import android.app.Application
-import android.arch.persistence.room.Room
-import com.android.example.github.db.GithubDb
-import com.android.example.github.db.RepoDao
-import com.android.example.github.db.UserDao
 import dagger.Module
-import dagger.Provides
-import javax.inject.Singleton
 
 @Module(includes = [ViewModelModule::class])
 class AppModule {
 
-    @Singleton
-    @Provides
-    fun provideDb(app: Application): GithubDb {
-        return Room
-            .databaseBuilder(app, GithubDb::class.java, "github.db")
-            .fallbackToDestructiveMigration()
-            .build()
-    }
-
-    @Singleton
-    @Provides
-    fun provideUserDao(db: GithubDb): UserDao {
-        return db.userDao()
-    }
-
-    @Singleton
-    @Provides
-    fun provideRepoDao(db: GithubDb): RepoDao {
-        return db.repoDao()
-    }
 }
